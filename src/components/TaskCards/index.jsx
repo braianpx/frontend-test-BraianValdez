4325ba9197d116eb3705cd1d93f0e945cdac078b
braianpx <braivaldez16@gmail.com>
2024-11-05 19:01:43 -0300

    update component TaskCards

--- src/components/TaskCards/index.jsx
+++ src/components/TaskCards/index.jsx
@@ -1,25 +1,30 @@
+import PropTypes from 'prop-types';
 import TaskCard from '../TaskCard/index'; 
-import { useTasks } from '../context/TaskContext';
 
-const TaskCards = () => {
-  const { tasks, loading, error } = useTasks();
+const TaskCards = ({ tasks, loading, error }) => {
 
   if (loading) return <p>Loading tasks...</p>;
   if (error) return <p>{error}</p>;
 
   return (
     <div className="flex flex-wrap justify-center gap-4 p-4">
-      {tasks.map((task) => (
+      {tasks?.map((task) => (
         <TaskCard 
-          key={task.id} // Asegúrate de que cada tarea tenga un `id` único
-          id={task.id}
-          title={task.title} 
-          description={task.description} 
-          completed={task.completed} 
+          key={task?.id} // Asegúrate de que cada tarea tenga un `id` único
+          id={task?.id}
+          title={task?.title} 
+          description={task?.description} 
+          completed={task?.completed} 
         />
       ))}
     </div>
   );
+}
+
+TaskCards.propTypes = {
+  tasks: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string.isRequired,
 }
 
 export default TaskCards;
