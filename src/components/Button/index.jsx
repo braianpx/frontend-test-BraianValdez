728f04276fdbc3de73b64feccac021c6df442070
braianpx <braivaldez16@gmail.com>
2024-11-06 09:33:35 -0300

    update styles css

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const Buttons = {
   primary: 'bg-primary w-auto px-4 py-2 rounded-lg shadow-md hover:bg-black-semi text-white',
-  secondary: 'bg-primary w-auto px-4 py-3 rounded-full shadow-md hover:bg-blue-800  text-white cursor-pointer text-lg',
+  secondary: 'bg-primary w-auto px-3 py-3 rounded-full shadow-md hover:bg-blue-800 text-white cursor-pointer text-lg',
   none:''
 } 
 
